98028833ed109ece8ac02cc3a68b9bf0d11d17d2
Noxwell <beebono@yahoo.com>
2025-03-14 17:45:28 -0500

    rework launch script, rework lib structure and add xcb

--- steamlink/src/gpucompat.c
+++ steamlink/src/gpucompat.c
@@ -1,93 +1,50 @@
 #define _GNU_SOURCE
 
 #include <dlfcn.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <sys/syscall.h>
-#include <drm/msm_drm.h>
-#include <drm/panfrost_drm.h>
 #include <linux/dma-heap.h>
-
-// Device path options
-#define DMA_HEAP_PATH "/dev/dma_heap/vidbuf_cached"
-#define REAL_DRM_DEVICE "/dev/dri/renderD128"
+#include <libavcodec/avcodec.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
 
 // Required fcntl.h defines
-#define O_RDWR      00000002
-#define O_CLOEXEC   02000000
-#define AT_FDCWD        -100
+#define O_RDWR        00000002
+#define O_CLOEXEC     02000000
+#define AT_FDCWD          -100
 
-// Static data stores
-static int (*real_ioctl)(int, unsigned long, ...);
-static char drm_dri_name[16];
+// Function stores
+int (*real_ioctl)(int, unsigned long, ...);
+int (*real_av_frame_ref)(AVFrame *, const AVFrame *);
 
-// DRM Driver version check
-int get_gpu_dri(int fd) {
-    struct drm_version version = {
-        .name = drm_dri_name,
-        .name_len = sizeof(drm_dri_name)
-    };
-    ioctl(fd, DRM_IOCTL_VERSION, &version);
-    if (drm_dri_name[0] == '\0') {
-        return -1;
-    }
-    return 0;
-}
-
-// DRM Device FD -> GEM Handle -> DRM Prime FD
-int drm_to_dma(int fd, size_t size) {
-    if (drm_dri_name[0] == '\0') {
-        if (get_gpu_dri(fd) != 0) {
-            return -1;
-        }
-    }
-
-    __u32 gem_handle = 0;
-    if (strcmp(drm_dri_name, "msm") == 0) {
-        struct drm_msm_gem_new gem = {
-            .size = size,
-            .flags = MSM_BO_WC
-        };
-        ioctl(fd, DRM_IOCTL_MSM_GEM_NEW, &gem);
-        gem_handle = gem.handle;
-    } else if (strcmp(drm_dri_name, "panfrost") == 0) {
-        struct drm_panfrost_create_bo gem = {
-            .size = size,
-            .flags = 0
-        };
-        ioctl(fd, DRM_IOCTL_PANFROST_CREATE_BO, &gem);
-        gem_handle = gem.handle;
-    } else {
-        return -1;
-    }
-
-    struct drm_prime_handle prime = {
-        .handle = gem_handle,
-        .flags = DRM_RDWR | DRM_CLOEXEC
-    };
-    ioctl(fd, DRM_IOCTL_PRIME_HANDLE_TO_FD, &prime);
-
-    return prime.fd;
-}
-
-// Actual syscall for open
+// Actual open syscall
 int real_open(const char *path, int flags, __u32 mode) {
     return syscall(SYS_openat, AT_FDCWD, path, flags, mode);
 }
 
-// Catch any calls that try to use DMA_HEAP/card0 and provide a device appropriate FD
+// Force success for DMA_HEAP access
 int open64(const char *path, int flags, __u32 mode) {
-    if (strcmp(path, DMA_HEAP_PATH) == 0) {
-        return real_open(REAL_DRM_DEVICE, flags, mode);
+    if (strcasestr(path, "/dev/dma_heap") != NULL) {
+        return 0;
     }
     return real_open(path, flags, mode);
 }
 
-// Because controls don't work unless this is routed this way
+// Initial open hook
 int open(const char *path, int flags, __u32 mode) {
     return open64(path, flags, mode);
+}
+
+// Software-backed "DMA-BUF" maker
+int get_shm_fd(size_t size) {
+    int fd = memfd_create("shm_buf", 0);
+    ftruncate(fd, (off_t)size);
+    return fd;
 }
 
 // Catch any calls that try to use DMA_HEAP and provide a usable DMA-BUF FD
@@ -99,7 +56,7 @@ int ioctl(int fd, unsigned long request, ...) {
 
     if (request == DMA_HEAP_IOCTL_ALLOC) {
         struct dma_heap_allocation_data *data = va_arg(args, struct dma_heap_allocation_data*);
-        data->fd = drm_to_dma(fd, data->len);
+        data->fd = get_shm_fd(data->len);
         va_end(args);
         return 0;
     }
@@ -108,4 +65,27 @@ int ioctl(int fd, unsigned long request, ...) {
     int ret = real_ioctl(fd, request, data);
     va_end(args);
     return ret;
+}
+
+// Convert any YUV420P frame to RGB24
+int av_frame_ref(AVFrame *dst, const AVFrame *src) {
+    if (!real_av_frame_ref) { real_av_frame_ref = dlsym(RTLD_NEXT, "av_frame_ref"); }
+
+    if (src->format == AV_PIX_FMT_YUV420P) {
+        AVFrame *rgb_src = av_frame_alloc();
+        rgb_src->width = src->width;
+        rgb_src->height = src->height;
+        rgb_src->format = AV_PIX_FMT_RGB0;
+        av_frame_get_buffer(rgb_src, 32);
+
+        struct SwsContext *sws_ctx = sws_getContext(
+            src->width, src->height, AV_PIX_FMT_YUV420P,
+            src->width, src->height, AV_PIX_FMT_RGB0,
+            SWS_FAST_BILINEAR | SWS_FULL_CHR_H_INT | SWS_ACCURATE_RND, NULL, NULL, NULL);
+        sws_scale(sws_ctx, (const uint8_t* const*)src->data, src->linesize, 0, src->height,
+                           rgb_src->data, rgb_src->linesize);
+
+        return real_av_frame_ref(dst, rgb_src);
+    }
+    return real_av_frame_ref(dst, src);
 }
